94dc0287517b9765c27d9053a8ae7d61f2fa33f3
Alex M <33159385+AlexM5150@users.noreply.github.com>
2022-02-21 21:04:09 -0800

    changes
    changes

--- sorting.hs
+++ sorting.hs
@@ -1,12 +1,14 @@
-quick :: (Ord a) => [a] -> [a]  
-quick [] = []  
-quick (x:xs) =   
-    let smaller = quick [a | a <- xs, a <= x]  
-        bigger = quick [a | a <- xs, a > x]  
-    in  smaller ++ [x] ++ bigger 
+module Main where
+    qsort :: (Ord a) => [a] -> [a]  
+    qsort [] = []  
+    qsort (x:xs) =   
+        let smaller = qsort [a | a <- xs, a <= x]  
+            bigger = qsort [a | a <- xs, a > x]  
+        in  smaller ++ [x] ++ bigger 
 
-merge :: Ord a => [a] -> [a] -> [a]
-merge xs [] = xs
-merge [] ys = ys
-merge (x:xs) (y:ys) | x <= y    = x:merge xs (y:ys)
-                    | otherwise = y:merge (x:xs) ys 
+    msort :: Ord a => [a] -> [a] -> [a]
+    msort xs [] = xs
+    msort [] ys = ys
+    msort (x:xs) (y:ys) | x <= y    = x:msort xs (y:ys)
+                        | otherwise = y:msort (x:xs) ys 
+Main qsort do [10,2,5,3,1,6,7,4,2,3,4,8,9]  [1,2,2,3,3,4,4,5,6,7,8,9,10]
